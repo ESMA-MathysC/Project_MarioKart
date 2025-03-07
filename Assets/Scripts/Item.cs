e9cacac9dcfe3d1a1c9c16298a37354545179409
ESMA-MathysC <m.cornuaud@rns.ecolescreatives.com>
2025-03-07 15:40:15 +0100

    07/03/2025 - Item Box and Items
    -Added item box prefab
    -Added item generation script
    -Added 3 items (green shell, mushroom and triple mushroom)

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Item : ScriptableObject
+{
+    public Sprite itemSprite;
+    public string itemName;
+    public int itemUseCount = 1;
+
+    public virtual void Activation(PlayerItemManager player)
+    {
+
+    }
+
+
+}
---
+++ Assets/Scripts/ItemLaunchable.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemLaunchable", menuName = "Scriptable Objects/ItemLaunchable")]
+public class ItemLaunchable : Item
+{
+    public GameObject objectToLaunch;
+
+    public override void Activation(PlayerItemManager player)
+    {
+        Debug.Log(objectToLaunch.name + "has been thrown");
+    }
+}
